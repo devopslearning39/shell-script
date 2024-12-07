db2d357d286fed96005b8a957b84f4e8eb9d37e3
Praveen Jella <praveen.jella@leap360.com>
2024-12-07 20:17:36 +0530

    intial commit

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -7,4 +7,4 @@ echo $SOURCE_DIR | ls -l
 # Showing log files more than 14 days
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
-echo "Files are hiving more than 14 days are : \n $FILES_TO_DELETE"
+echo "Files are hiving more than 14 days are : "\n $FILES_TO_DELETE
