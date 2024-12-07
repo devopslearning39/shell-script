2484fb9e3df5cecccaf17907cba673f4959a6d3d
Praveen Jella <praveen.jella@leap360.com>
2024-12-07 19:45:35 +0530

    intial commit

--- PastCreatedDateFile.sh
+++ PastCreatedDateFile.sh
@@ -2,12 +2,21 @@
 
 SOURCE_DIR='/Users/praveenjella/Documents/DEPLOYMENTS/Jella/Shell_Practice/test1'
 
+# Checking given filder/file is present or not
+
 if [ ! -d $SOURCE_DIR ] ; then
     mkdir $SOURCE_DIR
     echo "Mentioned directory is not there, so we have created"
 else
     echo "Directory is present"
 fi
+
+# Creating file/folder with the old/past date
+
+touch -d 19940612 jpDOB.txt
+mkdir -d 19930612 jaDOB.tx
+
+
 
 CHECK=(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
     # find . -type f -mtime +14 -name "*. log")
--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 
-touch -d 20201206 jp.txt
