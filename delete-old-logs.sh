d578996cf6d67356404f520574953c4e01b78ff5
Praveen Jella <praveen.jella@leap360.com>
2024-12-07 20:15:04 +0530

    intial commit

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -3,3 +3,8 @@
 SOURCE_DIR="/home/centos/shell-script"
 
 echo $SOURCE_DIR | ls -l
+
+# Showing log files more than 14 days
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+echo "$FILES_TO_DELETE"
