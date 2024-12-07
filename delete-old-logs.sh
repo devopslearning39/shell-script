5c6944cecfa811d661f3487251cd2be7c3010b9e
Praveen Jella <praveen.jella@leap360.com>
2024-12-07 19:54:12 +0530

    intial commit

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,13 +1,21 @@
-#/bin/bash
+#!/bin/bash
 
-SOURCE_DIR='/Users/praveenjella/Documents/DEPLOYMENTS/Jella/Shell_Practice/test1'
+SOURCE_DIR="/tmp/shellscript-logs"
 
-# Checking given filder/file is present or not
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-if [ ! -d $SOURCE_DIR ] ; then
-    mkdir $SOURCE_DIR
-    echo "Mentioned directory is not there, so we have created"
-else
-    echo "Directory is present"
+if [ ! -d $SOURCE_DIR ] # ! denotes opposite
+then
+    echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
 fi
 
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
+    rm -rf $line
+done <<< $FILES_TO_DELETE
