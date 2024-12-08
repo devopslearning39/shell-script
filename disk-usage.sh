73eb3fb1e05c0197acce9b052e6d750e2b3b8ab3
Praveen Jella <praveen.jella@leap360.com>
2024-12-08 12:21:28 +0530

    commit

---
+++ disk-usage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT)
+
+echo "$DISK_USAGE"
+
+
+
+# grep -vE 'tmp|File':
+    #  grep: This is used to search for text patterns in the input.
+    # -v: Inverts the match, meaning it excludes any lines that match the given pattern.
+    # -E: Enables extended regular expressions, allowing more complex patterns like | (OR).
+    # 'tmp|File': This is a regular expression pattern that matches any line containing either the word tmp or File. The pipe | works as an OR operator.
