781b54754cd934152d31a4b1579a6da7cf6eaab5
Praveen Jella <praveen.jella@leap360.com>
2024-12-08 12:45:04 +0530

    commit

--- disk-usage.sh
+++ disk-usage.sh
@@ -4,16 +4,21 @@ DISK_USAGE=$(df -hT | grep -vE "tmp|File")
 DISK_THRESHOLD=1
 MESSAGE=""
 
-echo "$DISK_USAGE"
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then
+        message+="High Disk Usage on $partition: $usage <br>"
+    fi
+done <<< $DISK_USAGE
 
-if [ $DISK_USAGE -ge $DISK_THRESHOLD ] ; then
-    echo "High disk usage, pls be caution"
-fi
+echo -e "Message: $message"
 
+#echo "$message" | mail -s "High Disk Usage" info@joindevops.com
 
-
-
-
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
 # grep -vE 'tmp|File':
     #  grep: This is used to search for text patterns in the input.
     # -v: Inverts the match, meaning it excludes any lines that match the given pattern.
