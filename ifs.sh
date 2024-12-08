798bec71cd082ba62cc9a633b4141d93f3bb73e9
Praveen Jella <praveen.jella@leap360.com>
2024-12-08 12:12:57 +0530

    commit

--- ifs.sh
+++ ifs.sh
@@ -8,7 +8,13 @@ else
     echo "File is there"
 fi
 
-while IFS= ":" read -r line
+while IFS= ":" read -r username password user_id group_id user_fullname home_dir shell_path
 do
-    echo "$line"
+    echo "Username: $username"
+    echo "Password: $password"
+    echo "userId: $user_id"
+    echo "groupId: $group_id"
+    echo "User full name": $user_fullname
+    echo "Home directory: $home_dir"
+    echo "Shell path: $shell_path"
 done < $FILE
